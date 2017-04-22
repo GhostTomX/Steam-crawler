726436f90ef2343214024de034e5934e3d4c861b
GhostTomX <r04522839@ntu.edu.tw>
2017-04-22 17:59:47 +0800

    更新v0.13 readme

--- main.js
+++ main.js
@@ -52,17 +52,18 @@ MongoClient.connect(url, function (err, db) {
                 var maxOnePage = $(".search_result_row").length;
                 for (var i = 0; i < maxOnePage; i++) //犯的错误 直接引用了  $(".search_result_row").length。 实际上由于我的删除动作这个值一直在减少
                 {
+                    try {
                     var data = $(".search_result_row")[0].attribs;//注意： 由于class属性是我不需要的，所以我在后文将其删除。因此每次查询得到的最新的一个$(".search_result_row") 就是 // 获得href,appid
                     data.spic = $(".search_result_row")[0].children[1].children[0].attribs.src;//最新的需要填充的。//这里是获得小图
                     data.title = $(".title")[i].children[0].data;//标题i保持不变.名称
                     try {
                         data.date = $(".search_released")[i].children[0].data;//发行日期
-                        console.log(data.date);
+                        // console.log(data.date);
                     } catch (error) {
                         try {
                             data.date = $(".search_released")[i].children[0].data;
                         } catch (error) {
-                            console.log("data error");
+                            console.log("date data error");
                             data.date = '发生错误';
                         }
                     }
@@ -71,7 +72,11 @@ MongoClient.connect(url, function (err, db) {
                     delete (data.onmouseout);
                     delete (data.class);
 
-                    db.collection("steam_info_20170422").insert(data);
+                    db.collection("steam_info_20170422_2").insert(data);
+                    } catch(error){
+                        console.log("其他data发生错误");
+                        continue;
+                    }
                 };
                 mySetTimeout(100);
                 console.log("第" + j + "页完成,当前并发数"+threadCount);
