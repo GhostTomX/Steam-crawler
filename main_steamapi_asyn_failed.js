289146bfe95f46ae34c325f09ae700e5b2a5a135
GhostTomX <r04522839@ntu.edu.tw>
2017-04-23 18:52:06 +0800

    尝试使用async去控制第二个异步函数，依然失败
    这个和上一个失败的主要原因是积攒的第二个异步太多了会丢失，仅剩下不定的几个（主线程的buffer会满导致丢失）。当j从1060-1070的时候就表现正常，但是从1000开始就会丢更别提从0开始了。看来大数量的异步编程会不能使用第一个里面的第二个异步

--- main_steamapi_asyn_failed.js
+++ main_steamapi_asyn_failed.js
@@ -9,10 +9,10 @@ console.log('爬虫程序开始运行......');
 
 MongoClient.connect(url, function (err, db) {
     console.log('数据库已链接');
-    var j = 0;
+    var j = 1060;
     lastPage = "";
     var jpool = [];
-    for (; j < 1080; j++) {
+    for (; j < 1070; j++) {
         jpool.push(j);
     }
     async.mapLimit(jpool, 5, function (j, cb) {
@@ -49,15 +49,16 @@ MongoClient.connect(url, function (err, db) {
                     return;
 
                 }
-
+                datagroup = [];
                 var maxOnePage = $(".search_result_row").length;
                 for (var i = 0; i < maxOnePage; i++) //犯的错误 直接引用了  $(".search_result_row").length。 实际上由于我的删除动作这个值一直在减少
                 {
+                   
                     // (function (i) {
                         try {
                             // var data = $(".search_result_row")[0].attribs;//注意： 由于class属性是我不需要的，所以我在后文将其删除。因此每次查询得到的最新的一个$(".search_result_row") 就是 // 获得href,appid
                             
-                             var data = $(".search_result_row")[i].attribs;//异步的的原因又可以变回i  异步每一次的tree都是新的
+                            var data = $(".search_result_row")[i].attribs;//异步的的原因又可以变回i  异步每一次的tree都是新的
                             data.spic = $(".search_result_row")[0].children[1].children[0].attribs.src;//最新的需要填充的。//这里是获得小图
                             data.title = $(".title")[i].children[0].data;//标题i保持不变.名称
                             data.price = $(".search_price")[i].children[0].data;//价格
@@ -72,19 +73,20 @@ MongoClient.connect(url, function (err, db) {
                                     data.date = '发生错误';
                                 }
                             }
-                            console.log("***" + data.title);
-                            (function (data){
-                            steamAPI(data['data-ds-appid'], function (steamapi) {
-                                // console.log("***" +i );
-                                data.type = steamapi.type;
-                                console.log("///" + data.title);
-                                delete (data.onmouseover);
-                                delete (data.onmouseout);
-                                delete (data.class);
-                                // db.collection("steam_info_20170423_2").insert(data);
+                            // console.log("***" + data.title);
+                        //     (function (data){
+                        //     steamAPI(data['data-ds-appid'], function (steamapi) {
+                        //         // console.log("***" +i );
+                        //         data.type = steamapi.type;
+                        //         console.log("///" + data.title);
+                        //         delete (data.onmouseover);
+                        //         delete (data.onmouseout);
+                        //         delete (data.class);
+                        //         // db.collection("steam_info_20170423_2").insert(data);
 
-                            });
-                        })(data);
+                        //     });
+                        // })(data);
+                        datagroup.push(data);
                         } catch (error) {
 
                             console.log("其他data发生错误");
@@ -93,6 +95,24 @@ MongoClient.connect(url, function (err, db) {
                         }
                     // })(i);
                 };
+                console.log(datagroup.length);
+                var position = 0
+                async.eachSeries(datagroup,function(data,cb){
+                    steamAPI(data['data-ds-appid'], function (steamapi) {
+                                position++;
+                                data.type = steamapi.type;
+                                console.log("process//" + j +"::"+position);
+                                delete (data.onmouseover);
+                                delete (data.onmouseout);
+                                delete (data.class);
+                                cb(err);
+                                db.collection("steam_info_20170423_2").insert(data);
+                                
+                }
+            );},function(err){
+                console.log(err);
+            });
+
                 mySetTimeout(100);
                 console.log("--------------------------第" + j + "页完成,当前并发数" + threadCount);
                 j++;
